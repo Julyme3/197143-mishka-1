a60d4bc44fc14648f038c90a3e16b0fea9890bde
JuliaGord <jg0311@bk.ru>
2017-03-27 21:39:58 +0300

    Верстка со стайлгайдом

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,19 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
     .pipe(plumber())
-    .pipe(sass({
-      includePaths: require('node-normalize-scss').includePaths
-  }))
+    .pipe(sass())
     .pipe(postcss([
       autoprefixer({browsers: [
         "last 2 versions"
       ]})
     ]))
-    .pipe(gulp.dest("css"))
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
@@ -34,4 +34,14 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("symbols", function() {
+  return gulp.src("img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+}))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("img"));
 });
