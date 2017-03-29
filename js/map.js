4a67127a1cf76bd9cdbc2c3439dd0067421cc9ee
JuliaGord <jg0311@bk.ru>
2017-03-29 08:41:49 +0300

    Релизована сборка проекта

--- js/map.js
+++ js/map.js
@@ -1,15 +1,15 @@
 ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
+    var myMap = new ymaps.Map("map", {
             center: [59.93860683861325,30.322990126983658],
             zoom: 16
         }, {
-            searchControlProvider: 'yandex#search'
+            searchControlProvider: "yandex#search"
         }),
         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
             hintContent: "Офис 101",
             balloonContent: "Офис 101"
         }, {
-            iconLayout: 'default#image',
+            iconLayout: "default#image",
             // Своё изображение иконки метки.
             iconImageHref: "./img/icon-map-pin.svg",
             // Размеры метки.
